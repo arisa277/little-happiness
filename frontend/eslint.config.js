08ffb24136cf00029a3abe7d9ee1e605ccb522ef
arisa277 <ars.n.277@gmail.com>
2024-09-04 22:59:25 +0900

    setup frontend / backend

---
+++ frontend/eslint.config.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    rules: {
+      // カスタムルールを追加する場合はここに記述
+    },
+  },
+];
