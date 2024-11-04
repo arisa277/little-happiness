08cf01aae6c380e0be78dd02f992079d86ad6e20
arisa277 <ars.n.277@gmail.com>
2024-11-04 17:46:34 +0900

    update vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/little-happiness/',
   plugins: [react()],
   build: {
     outDir: 'dist'  // ビルド出力先を dist に指定
